4fb3812c6ee3e44ab5215e9cab681b1c610b0fbc
Akash Kumar Singh <akkisingh952380@gmail.com>
2025-12-22 12:55:22 +0530

    minor change in email.js file

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -8,6 +8,9 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
+  },
+  tls:{
+    rejectUnauthorized:false //fix for self-signed cert
   }
 });
 
